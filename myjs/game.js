f963ab71ccc57579fb709041e02cdd3e6a4b9a21
Artour Konstantinov <artoukons@gmail.com>
2021-04-04 19:02:33 -0400

    sprite loaded

--- myjs/game.js
+++ myjs/game.js
@@ -1,24 +1,15 @@
-/*
-let type = "WebGL"
-if(!PIXI.utils.isWebGLSupported()){
-  type = "canvas"
-}
-
-PIXI.utils.sayHello(type)
-//*/
-
 //Create a Pixi Application
 let app = new PIXI.Application({ 
     width: 512,         // default: 800
     height: 512,        // default: 600
     antialias: true,    // default: false
     transparent: false, // default: false
-    //forceCanvas: true,
+    forceCanvas: true, //"fixes" cross origin issues.
     resolution: 1       // default: 1
   }
 );
 
-//app.renderer.backgroundColor = 0x061639;
+app.renderer.backgroundColor = 0xffffff;
 //app.renderer.autoResize = true;
 //app.renderer.resize(512, 512);
 //app.renderer.view.style.position = "absolute";
@@ -27,3 +18,21 @@ let app = new PIXI.Application({
 
 //Add the canvas that Pixi automatically created for you to the HTML document
 document.body.appendChild(app.view);
+
+
+PIXI.loader
+  .add([{
+  	name: "boy",
+    url: "img/boy.png",
+    crossOrigins: true,
+    onComplete: function () {}
+  }])
+  .load(setup);
+  
+function setup() {
+	  //Create the cat sprite
+	  let boy = new PIXI.Sprite(PIXI.loader.resources["boy"].texture);
+	  
+	  //Add the cat to the stage
+	  app.stage.addChild(boy);
+}
